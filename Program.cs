aa4ddd09a12d94940021e2871d65e8927bf80177
luanbalbi <luanbalbi@gmail.com.br>
2024-08-26 08:37:23 -0300

    FEAT: Add Smartphone, Nokia and Iphone classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "21999999999", modelo: "Modelo A", imei: "7894561320", memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "11888888888", modelo: "Modelo B", imei: "1023456789", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
